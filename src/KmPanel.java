28b9d07e2d70f88835acadc9eaf9b89d4627dcbb
fuseh <fuseh@DESKTOP-43G7UB0>
2018-01-31 13:51:59 +0900

    Km変更

--- src/KmPanel.java
+++ src/KmPanel.java
@@ -15,14 +15,16 @@ public class KmPanel extends JPanel implements ActionListener {
 
 	private MainFrame frame;
 	private MariaDbConnector m;
-	private JButton Mbutton[] = new JButton[5];
+	private JButton Mbutton[] = new JButton[5]; //メニューボタン
 	private JButton Kbutton[] = new JButton[3];// 決定ボタン
-	private JLabel label[] = new JLabel[6];
+	private JLabel label[] = new JLabel[6]; //ラベル
 	private JLabel Klabel[] = new JLabel[5]; // 空白用ラベル
 	private JTextField text[] = new JTextField[10];// 入力
-	private JPanel p[] = new JPanel[6];
-	private int Itext0,Itext1,Jikyu;
-	private String Stext0,Stext1,Sjikyu;
+	private JPanel p[] = new JPanel[6]; //パネル用
+	private int Itext[] = new int[10]; //整数変換用変数
+	private int Stext[] = new int[10]; //文字列変換用変数
+	private int Jikyu;
+	private String Sjikyu;
 
 	public KmPanel(MainFrame frame) {
 
@@ -31,7 +33,7 @@ public class KmPanel extends JPanel implements ActionListener {
 
 		setLayout(new BorderLayout());
 
-		Mbutton[0] = new JButton("Button3");
+		Mbutton[0] = new JButton();
 		Mbutton[0].setText("<html>メイン画面に戻る</html>");
 		Mbutton[0].setFont(new Font("ＭＳ ゴシック", Font.BOLD, 35));
 		Mbutton[0].addActionListener(this);
@@ -43,49 +45,103 @@ public class KmPanel extends JPanel implements ActionListener {
 		label[0].setBackground(Color.BLACK);
 		label[0].setOpaque(true);
 
-		label[1] = new JLabel("勤務開始時間");
+		label[1] = new JLabel("勤務時間");
 		label[1].setFont(new Font("ＭＳ ゴシック", 0, 30));
 		label[1].setHorizontalAlignment(SwingConstants.CENTER);
 		label[1].setOpaque(true);
 
+		//文字列を整数変換
+		//勤務時間用テキストフィールド
 		text[0] = new JTextField("0");
-		Stext0 = text[0].getText();
-		Itext0 = Integer.parseInt(Stext0);
 
-		label[2] = new JLabel("勤務終了時間");
+		label[2] = new JLabel("時");
 		label[2].setFont(new Font("ＭＳ ゴシック", 0, 30));
 		label[2].setHorizontalAlignment(SwingConstants.CENTER);
 		label[2].setOpaque(true);
 
 		text[1] = new JTextField("0");
-		Stext1 = text[1].getText();
-		Itext1 = Integer.parseInt(Stext1);
 
-		Klabel[0] = new JLabel("");
-
-		label[3] = new JLabel("休憩時間");
+		label[3] = new JLabel("分");
 		label[3].setFont(new Font("ＭＳ ゴシック", 0, 30));
 		label[3].setHorizontalAlignment(SwingConstants.CENTER);
 		label[3].setOpaque(true);
 
-		text[2] = new JTextField();
-
-		label[4] = new JLabel("時給");
+		label[4] = new JLabel("勤務時間外");
 		label[4].setFont(new Font("ＭＳ ゴシック", 0, 30));
 		label[4].setHorizontalAlignment(SwingConstants.CENTER);
 		label[4].setOpaque(true);
 
 		text[3] = new JTextField("0");
-		Sjikyu = text[3].getText();
-		Jikyu = Integer.parseInt(Sjikyu);
 
-		label[5] = new JLabel("日給の計算結果");
+		label[5] = new JLabel("時");
 		label[5].setFont(new Font("ＭＳ ゴシック", 0, 30));
 		label[5].setHorizontalAlignment(SwingConstants.CENTER);
 		label[5].setOpaque(true);
 
+		text[4] = new JTextField("0");
 
-		Kbutton[0] = new JButton("決定");
+		label[6] = new JLabel("分");
+		label[6].setFont(new Font("ＭＳ ゴシック", 0, 30));
+		label[6].setHorizontalAlignment(SwingConstants.CENTER);
+		label[6].setOpaque(true);
+
+		label[7] = new JLabel("勤務時間外倍率");
+		label[7].setFont(new Font("ＭＳ ゴシック", 0, 30));
+		label[7].setHorizontalAlignment(SwingConstants.CENTER);
+		label[7].setOpaque(true);
+
+		text[5] = new JTextField("0");
+
+		label[8] = new JLabel("倍");
+		label[8].setFont(new Font("ＭＳ ゴシック", 0, 30));
+		label[8].setHorizontalAlignment(SwingConstants.CENTER);
+		label[8].setOpaque(true);
+
+		Klabel[0] = new JLabel("");
+		Klabel[0].setFont(new Font("ＭＳ ゴシック", 0, 30));
+		Klabel[0].setHorizontalAlignment(SwingConstants.CENTER);
+		Klabel[0].setOpaque(true);
+
+		Klabel[1] = new JLabel("");
+		Klabel[1].setFont(new Font("ＭＳ ゴシック", 0, 30));
+		Klabel[1].setHorizontalAlignment(SwingConstants.CENTER);
+		Klabel[1].setOpaque(true);
+
+		label[9] = new JLabel("休憩時間");
+		label[9].setFont(new Font("ＭＳ ゴシック", 0, 30));
+		label[9].setHorizontalAlignment(SwingConstants.CENTER);
+		label[9].setOpaque(true);
+
+		text[6] = new JTextField("0");
+
+		label[10] = new JLabel("時");
+		label[10].setFont(new Font("ＭＳ ゴシック", 0, 30));
+		label[10].setHorizontalAlignment(SwingConstants.CENTER);
+		label[10].setOpaque(true);
+
+		text[7] = new JTextField("0");
+
+		label[11] = new JLabel("分");
+		label[11].setFont(new Font("ＭＳ ゴシック", 0, 30));
+		label[11].setHorizontalAlignment(SwingConstants.CENTER);
+		label[11].setOpaque(true);
+
+		label[12] = new JLabel("時給");
+		label[12].setFont(new Font("ＭＳ ゴシック", 0, 30));
+		label[12].setHorizontalAlignment(SwingConstants.CENTER);
+		label[12].setOpaque(true);
+
+		text[8] = new JTextField("0");
+
+		label[13] = new JLabel("円");
+		label[13].setFont(new Font("ＭＳ ゴシック", 0, 30));
+		label[13].setHorizontalAlignment(SwingConstants.CENTER);
+		label[13].setOpaque(true);
+
+		Klabel[2] = new JLabel("");
+		Klabel[2].setFont(new Font("ＭＳ ゴシック", 0, 30));
+		Klabel[2].setHorizontalAlignment(SwingConstants.CENTER);
+		Klabel[2].setOpaque(true);
 
 		// パネル設定
 
@@ -136,14 +192,6 @@ public class KmPanel extends JPanel implements ActionListener {
 		if (object == Mbutton[0]) {
 			MainPanel panel = new MainPanel(this.frame);
 			frame.changeWindow(panel);
-		}
-		if(object == Kbutton[0]) {
-			int nikyu = 0;
-			int roudou = 0;
-			roudou = Itext1 - Itext0;
-			nikyu = roudou * Jikyu;
-			System.out.println(nikyu);
-
 		}
 	}
 }
